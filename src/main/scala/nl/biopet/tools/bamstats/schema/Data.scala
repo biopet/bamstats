7ccce224cc124e91974ac3be634e957c653a1129
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-07-02 16:16:54 +0200

    Histograms are doubleArrays

--- src/main/scala/nl/biopet/tools/bamstats/schema/Data.scala
+++ src/main/scala/nl/biopet/tools/bamstats/schema/Data.scala
@@ -1,31 +1,33 @@
 package nl.biopet.tools.bamstats.schema
 
-case class Data(mappingQualityHistogram: Map[Int, Long],
-                insertSizeHistogram: Map[Int, Long],
-                clippingHistogram: Map[Int, Long],
-                leftClippingHistogram: Map[Int, Long],
-                rightClippingHistogram: Map[Int, Long],
-                _5_ClippingHistogram: Map[Int, Long],
-                _3_ClippingHistogram: Map[Int, Long]) {
+import nl.biopet.utils.Counts
 
-  def +(other: Data): Data = {
-    new Data(
-      mappingQualityHistogram = addHistogram(this.mappingQualityHistogram,
-                                             other.mappingQualityHistogram),
-      insertSizeHistogram =
-        addHistogram(this.insertSizeHistogram, other.insertSizeHistogram),
-      clippingHistogram =
-        addHistogram(this.clippingHistogram, other.clippingHistogram),
-      leftClippingHistogram =
-        addHistogram(this.leftClippingHistogram, other.leftClippingHistogram),
-      rightClippingHistogram =
-        addHistogram(this.rightClippingHistogram, other.rightClippingHistogram),
-      _5_ClippingHistogram =
-        addHistogram(this._5_ClippingHistogram, other.rightClippingHistogram),
-      _3_ClippingHistogram =
-        addHistogram(this._3_ClippingHistogram, other._3_ClippingHistogram)
-    )
-  }
+case class Data(mappingQualityHistogram: Counts.DoubleArray[Int],
+                insertSizeHistogram: Counts.DoubleArray[Int],
+                clippingHistogram: Counts.DoubleArray[Int],
+                leftClippingHistogram: Counts.DoubleArray[Int],
+                rightClippingHistogram: Counts.DoubleArray[Int],
+                _5_ClippingHistogram: Counts.DoubleArray[Int],
+                _3_ClippingHistogram: Counts.DoubleArray[Int]) {
+
+//  def +(other: Data): Data = {
+//    new Data(
+//      mappingQualityHistogram = addHistogram(this.mappingQualityHistogram,
+//                                             other.mappingQualityHistogram),
+//      insertSizeHistogram =
+//        addHistogram(this.insertSizeHistogram, other.insertSizeHistogram),
+//      clippingHistogram =
+//        addHistogram(this.clippingHistogram, other.clippingHistogram),
+//      leftClippingHistogram =
+//        addHistogram(this.leftClippingHistogram, other.leftClippingHistogram),
+//      rightClippingHistogram =
+//        addHistogram(this.rightClippingHistogram, other.rightClippingHistogram),
+//      _5_ClippingHistogram =
+//        addHistogram(this._5_ClippingHistogram, other.rightClippingHistogram),
+//      _3_ClippingHistogram =
+//        addHistogram(this._3_ClippingHistogram, other._3_ClippingHistogram)
+//    )
+//  }
   def addHistogram(map1: Map[Int, Long],
                    map2: Map[Int, Long]): Map[Int, Long] = {
     {
