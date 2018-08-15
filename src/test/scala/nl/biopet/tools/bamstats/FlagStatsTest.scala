ba2415eebc690d3cc59b24db79ac57aaa0a0e479
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-08-15 10:22:19 +0200

    add test file

---
+++ src/test/scala/nl/biopet/tools/bamstats/FlagStatsTest.scala
@@ -0,0 +1,11 @@
+package nl.biopet.tools.bamstats
+
+import org.scalatest.Matchers
+import org.scalatest.testng.TestNGSuite
+import org.testng.annotations.Test
+
+class FlagStatsTest extends TestNGSuite with Matchers {
+
+  @Test
+  def dummyTest(): Unit = ???
+}
