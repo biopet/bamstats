a86dcc1cda59af5b2bf3f4f0e1665094a85ff4d8
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-08-28 09:18:25 +0200

    move pacakage

--- src/test/scala/nl/biopet/tools/bamstats/schema/FlagStatsDataTest.scala
+++ src/test/scala/nl/biopet/tools/bamstats/schema/FlagStatsDataTest.scala
@@ -19,7 +19,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package nl.biopet.tools.bamstats.generate.schema
+package nl.biopet.tools.bamstats.schema
 
 import htsjdk.samtools.{SAMRecord, SamReader, SamReaderFactory}
 import nl.biopet.test.BiopetTest
