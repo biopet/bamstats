8dc0c23cc6b60e071c8c808ac21a557576f7b19f
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-08-30 08:20:02 +0200

    add header

--- src/main/scala/nl/biopet/tools/bamstats/merge/Merge.scala
+++ src/main/scala/nl/biopet/tools/bamstats/merge/Merge.scala
@@ -44,6 +44,7 @@ object Merge extends ToolCommand[Args] {
   def descriptionText: String =
     """
       |This module will merge bamstats files together and keep the sample/library/readgroup structure.
+      |It will also validate the resulting file.
       """.stripMargin
 
   def manualText: String =
