ad4d65987d785873cbbf6dc434d8e7fafc9b6026
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-09-03 11:39:57 +0200

    rewrite scatter and merge test

--- src/test/scala/nl/biopet/tools/bamstats/ScatterAndMergeTest.scala
+++ src/test/scala/nl/biopet/tools/bamstats/ScatterAndMergeTest.scala
@@ -24,102 +24,30 @@ package nl.biopet.tools.bamstats
 import java.io.File
 
 import nl.biopet.test.BiopetTest
-import nl.biopet.tools.bamstats.schema.BamstatsRoot
-import nl.biopet.utils.ngs.intervals.BedRecordList
-import org.testng.annotations.{AfterClass, Test}
-import nl.biopet.tools.scatterregions.ScatterRegions.scatterRegions
-import scala.collection.mutable.ListBuffer
+import nl.biopet.tools.bamstats.generate.Generate.{
+  extractStats,
+  extractStatsRegion,
+  extractStatsUnmappedReads
+}
+import nl.biopet.utils.ngs.intervals.{BedRecord, BedRecordList}
+import org.testng.annotations.Test
 
 class ScatterAndMergeTest extends BiopetTest {
-
-  val testDirs: ListBuffer[File] = ListBuffer()
-  def tempDir(prefix: String, suffix: String): File = {
-    val dir = File.createTempFile(prefix, suffix)
-    dir.delete()
-    dir.mkdirs()
-    testDirs += dir
-    dir
-  }
-
-  def mkdir(file: File): File = {
-    file.delete()
-    file.mkdirs()
-    file
-  }
-
-  @AfterClass
-  def deleteTestDirs(): Unit = {
-    testDirs.toList.foreach { dir =>
-      dir.delete()
-    }
-  }
-
-  val bamFile: File = resourceFile("/fake_chrQ1000simreads.bam").getAbsoluteFile
-  val referenceFile: File = resourceFile("/fake_chrQ.fa").getAbsoluteFile
-
-  val bamStatsGenerateArguments: Array[String] = Array[String](
-    "generate",
-    "-b",
-    bamFile.toString,
-    "--sample",
-    "sample",
-    "--library",
-    "library",
-    "--readgroup",
-    "readgroup",
-    "--reference",
-    referenceFile.toString
-  )
   @Test
   def scattersWithBed(): Unit = {
-    val outputDir = tempDir("scatterAndMerge", ".d")
-    val scattersDir = mkdir(new File(outputDir, "scatters"))
-    scatterRegions(referenceFile, scattersDir, scatterSize = 5000)
-    val scatterFiles = scattersDir.listFiles()
-    val outputDirs =
-      scatterFiles.map(file => new File(outputDir, s"${file.getName}.d"))
-    outputDirs.foreach(mkdir)
-
-    scatterFiles.zip(outputDirs).foreach {
-      case (bedFile, bedOutputDir) =>
-        BamStats.main(
-          bamStatsGenerateArguments ++ Array("--bedFile",
-                                             bedFile.toString,
-                                             "--outputDir",
-                                             bedOutputDir.toString,
-                                             "--scatterMode"))
+    val bamFile
+      : File = resourceFile("/fake_chrQ1000simreads.bam").getAbsoluteFile
+    val referenceFile: File = resourceFile("/fake_chrQ.fa").getAbsoluteFile
+    val regions: List[BedRecord] =
+      BedRecordList.fromReference(referenceFile).scatter(5000).flatten
+    val regionStats: List[GroupStats] = regions.map { region =>
+      extractStatsRegion(bamFile, region, scatterMode = true)
     }
+    val unmappedStats: GroupStats = extractStatsUnmappedReads(bamFile)
 
-    // Get unmapped reads as well
-    val unmappedDir = mkdir(new File(outputDir, "unmapped.d"))
-    BamStats.main(
-      bamStatsGenerateArguments ++ Array("--onlyUnmapped",
-                                         "--outputDir",
-                                         unmappedDir.toString))
-
-    val bamStatsFiles: Seq[File] = outputDirs.map(dir =>
-      new File(dir, "bamstats.json")) ++ Seq(
-      new File(unmappedDir, "bamstats.json"))
-
-    val mergedBamstatsFile = new File(outputDir, "merged_bamstats.json")
-
-    val inputBamStats: Array[String] =
-      bamStatsFiles.flatMap(file => Seq("-i", file.toString)).toArray
-    BamStats.main(
-      Array("merge") ++ inputBamStats ++ Array("-o",
-                                               mergedBamstatsFile.toString))
-
-    val completeBamstatsDir: File = mkdir(
-      new File(outputDir, "complete_bamstats.d"))
-
-    val completeBamstatsFile = new File(completeBamstatsDir, "bamstats.json")
-    BamStats.main(
-      bamStatsGenerateArguments ++ Array("--outputDir",
-                                         completeBamstatsDir.toString))
-
-    // Since doubleArrays do not have a fixed order, it is the stats which need to be compared.
-    val completeStats = BamstatsRoot.fromFile(completeBamstatsFile)
-    val mergedStats = BamstatsRoot.fromFile(mergedBamstatsFile)
-    mergedStats == completeStats shouldBe true
+    val mergedStats: GroupStats =
+      (regionStats ++ List(unmappedStats)).reduce(_ += _)
+    val totalStats: GroupStats = extractStats(bamFile)
+    mergedStats shouldBe totalStats
   }
 }
