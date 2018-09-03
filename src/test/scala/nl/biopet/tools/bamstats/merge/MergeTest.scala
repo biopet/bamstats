286b4bde4509adcfbde9dd05e7090087ad95c1d3
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-09-03 11:43:30 +0200

    add merge test framework

---
+++ src/test/scala/nl/biopet/tools/bamstats/merge/MergeTest.scala
@@ -0,0 +1,14 @@
+package nl.biopet.tools.bamstats.merge
+
+import nl.biopet.test.BiopetTest
+
+class MergeTest extends BiopetTest {
+
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      Merge.main(Array())
+    }
+  }
+
+
+}
