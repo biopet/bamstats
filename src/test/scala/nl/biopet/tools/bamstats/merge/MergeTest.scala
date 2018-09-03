674ba56a51628cf50fac11ef607d391b23fcf83b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-09-03 15:26:26 +0200

    add MergeTest.scala

--- src/test/scala/nl/biopet/tools/bamstats/merge/MergeTest.scala
+++ src/test/scala/nl/biopet/tools/bamstats/merge/MergeTest.scala
@@ -1,13 +1,39 @@
 package nl.biopet.tools.bamstats.merge
 
+import java.io.File
+
 import nl.biopet.test.BiopetTest
+import nl.biopet.tools.bamstats.schema.BamstatsRoot
+import org.testng.annotations.Test
 
 class MergeTest extends BiopetTest {
 
+  @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
       Merge.main(Array())
     }
   }
 
+  @Test
+  def testMergeFile(): Unit = {
+    val scatteredStats = List(
+      resourceFile("/stats/scatter-0/bamstats.json"),
+      resourceFile("/stats/scatter-1/bamstats.json"),
+      resourceFile("/stats/scatter-2/bamstats.json"),
+      resourceFile("/stats/unmapped/bamstats.json")
+    )
+
+    val totalStatsFile = resourceFile("/stats/complete/bamstats.json")
+    val mergedOutputFile = File.createTempFile("mergedStats", ".json")
+
+    val inputStats: Array[String] = scatteredStats
+      .flatMap(statsFile => Array("-i", statsFile.getAbsolutePath))
+      .toArray
+
+    Merge.main(inputStats ++ Array("-o", mergedOutputFile.getAbsolutePath))
+
+    BamstatsRoot.fromFile(mergedOutputFile) == BamstatsRoot.fromFile(
+      totalStatsFile) shouldBe true
+  }
 }
